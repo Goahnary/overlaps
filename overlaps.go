7b29a40060c6e951a65e1335bcd6d8676fdcacb9
Noah Gary <noah.gary@healthcareassetnetwork.com>
2020-03-06 17:05:09 -0500

    recursively calling the function now. Not working as intended. Only comparing first item in event slice. WIP.

--- overlaps.go
+++ overlaps.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	// "reflect"
 )
+
+// type m = map[string]interface{}
 
 type event struct {
 	start int
 	end int
-	groups []string
+	groups map[string]bool
 }
 
 func main(){
@@ -17,17 +19,30 @@ func main(){
 		event{
 			start: 0,
 			end: 4,
-			groups: []string{"apple sale"},
+			groups: map[string]bool{
+				"apple sale": true,
+			},
 		},
 		event{
 			start: 2,
 			end: 5,
-			groups: []string{"banna sale"},
+			groups: map[string]bool{			
+				"banna sale": true,			
+			},
 		},
 		event{
 			start: 3,
 			end: 10,
-			groups: []string{"car sale"},
+			groups: map[string]bool{
+				"car sale": true,
+			},
+		},
+		event{
+			start: 11,
+			end: 20,
+			groups: map[string]bool{
+				"car sale": true,
+			},
 		},
 		/*
 		event{
@@ -48,7 +63,7 @@ func main(){
 		*/
 	}
 
-	overlaps := findOverlaps(campaigns)
+	overlaps := recursiveOverlaps(campaigns, make([]event, 0))
 
 	fmt.Printf("\n-------------\n| Overlaps  |\n-------------\n\n")
 
@@ -57,51 +72,106 @@ func main(){
 	}
 }
 
-func findOverlaps(campaigns []event) []event {
+func recursiveOverlaps(events []event, overlaps []event) []event {
+	//Remove event, that we shall call "comparisonEvent", and compare to the rest of the events to find overlaps.
+	//Throw that event away and repeat this process.
+	//base case: events array is of length 2. return comparisonEvent
 
-	//return array
-	overlaps := make([]event, 0)
+	//pop comparisonEvent (first item in array)
+	comparisonEvent, events := events[len(events)-1], events[:len(events)-1]
 
-	fmt.Printf("-------------\n| Campaigns |\n-------------\n\n")
+	if len(events) == 0 {
+		return overlaps;
+	}
+	for _, eventItem := range events {
 
-	//if(campaigns == nil){ //base case
-	//	return overlaps
-	//} else {
-		//compare events
-		for _, campA := range campaigns {
-			for _, campB := range campaigns {
-				if campB.start < campA.end && !reflect.DeepEqual(campA.groups, campB.groups) && campA.start <= campB.start{ //There's an overlap!
-					//one encompasses another
-					if campA.end > campB.end { //campA encompasses campB
-						//append names and make it unique
-						groups := appendUnique(campA.groups, campB.groups)
-						overlaps = append( overlaps, event{campB.start, campB.end, groups})
-					} else {
-						//append names and make it unique
-						groups := appendUnique(campA.groups, campB.groups)
-						overlaps = append( overlaps, event{campB.start, campA.end, groups } )
-					}
-				}
+		if 	overlapExists(comparisonEvent, eventItem) {
+
+			if comparisonEvent.end > eventItem.end { //comparisonEvent encompasses eventItem
+
+				//append names and make it unique
+				groups := mergeKeys(comparisonEvent.groups, eventItem.groups)
+
+				//add to overlaps
+				overlaps = append( overlaps, event{eventItem.start, eventItem.end, groups} )
+
+				//call the function again
+				return recursiveOverlaps(events, overlaps)
+
+			} else {
+
+				//append names and make it unique
+				groups := mergeKeys(comparisonEvent.groups, eventItem.groups)
+
+				//add to overlaps
+				overlaps = append( overlaps, event{eventItem.start, comparisonEvent.end, groups } )
+
+				return recursiveOverlaps(events, overlaps)
+
 			}
-			fmt.Printf("Campaign %v\nStart:%v\nEnd:%v\n\n", campA.groups, campA.start, campA.end)
 		}
-		//return append(overlaps, findOverlaps(overlaps)...)
-		return overlaps
-	//}
+	}
+
+	// for _, c := range events {
+	// 	fmt.Printf("Campaign %v\nStart:%v\nEnd:%v\n\n", c.groups, c.start, c.end)
+	// }
+
+	// fmt.Printf("Overlap between event 1&2? %v", overlapExists(events[0],events[0]))
+
+	// literalEvent := []event{ event{len(events),0,map[string]bool{"Error: Nil Case Reached. Length of Events in start of object.":true}} }
+	return nil
 }
 
-func appendUnique(a []string, b []string) []string {
-
-	check := make(map[string]int)
-	d := append(a, b...)
-	res := make([]string,0)
-	for _, val := range d {
-		check[val] = 1
+func overlapExists(a event, b event) bool {
+	if a.end > b.start && !groupsEqual(a.groups, b.groups) {
+		fmt.Printf("========================\n")
+		fmt.Printf("End of \"a\" before start of \"b\"? %v\n", true)
+		fmt.Printf("a & b groups the same? %v\n", false)
+		fmt.Printf("a groups: %v\n", a.groups)
+		fmt.Printf("b groups: %v\n", b.groups)
+		fmt.Printf("========================\n")
 	}
 
-	for letter, _ := range check {
-		res = append(res,letter)
+
+	fmt.Printf("%v\n", a)
+	fmt.Printf("%v\n", b)
+
+	if a.start <= b.start {
+		fmt.Printf("Start of a before start of b? %v\n", true)
+	} else {
+		fmt.Printf("%v !<= %v\n", a.start, b.start)
+	}
+	return true
+}
+
+func groupsEqual(a map[string]bool, b map[string]bool) bool {
+	aIsInB := true
+	bIsInA := true
+
+	//are all items in a in b?
+	for key, _ := range a {
+		if !b[key]{ //if I get a false value then all values in a are not in b
+			aIsInB = false
+		}
 	}
 
-	return res
+	//are all items in b in a?
+	for key, _ := range b {
+		if !a[key]{ //if I get a false value then all values in b are not in a
+			bIsInA = false
+		}
+	}
+
+	isEqual := aIsInB && bIsInA // true if both contain all elements of eachother
+	return isEqual
+
+
+}
+
+// Given two maps, recursively merge right into left, NEVER replacing any key that already exists in left
+func mergeKeys(left, right map[string]bool) map[string]bool {
+    for key, rightVal := range right {
+        left[key] = rightVal
+    }
+    return left
 }
